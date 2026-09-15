f7a0650054e7fb75fafc3c69a6509411fef1b4c9
Carl <cayboencarl36254@gmail.com>
2026-09-15 14:42:22 +0800

    g

---
+++ level-1-kotlin-fundamentals/DataTypes.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val age: Int = 20
+    val height: Double = 5.4
+    val schoolName: String = "UC"
+    val isOnline: Boolean = true
+
+    println("I am $age years old, \n$height in height. \nStudying in $schoolName, \ncurrently $isOnline")
+
+
+}
