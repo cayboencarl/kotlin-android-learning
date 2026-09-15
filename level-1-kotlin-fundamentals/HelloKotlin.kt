3b4a9cb67363fa0b6cc6fb4b8bca8be36dc52707
Carl <cayboencarl36254@gmail.com>
2026-09-15 14:21:02 +0800

    g

---
+++ level-1-kotlin-fundamentals/HelloKotlin.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val myName = "Carl Cayboen"
+    val birthDate = 2006
+    var currentlyDoing: String = "Learning Kotlin"
+
+println("My name is $myName and I was born on $birthDate. I am currently $currentlyDoing")
+currentlyDoing = "Eating Fries"
+println("Now I'm $currentlyDoing")
+
+}
